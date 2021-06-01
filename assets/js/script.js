2d6022dc48c60beb9994c9895dc7582ee5658709
Diones P. Alves <36514458+Diones25@users.noreply.github.com>
2021-05-31 22:03:50 -0300

    Adicionando modal e o responsivo do site

--- assets/js/script.js
+++ assets/js/script.js
@@ -39,3 +39,25 @@ function menuMobile() {
       x.style.display = "block";
     }
 }
+
+/*Script Modal*/
+function openModal(mn) {
+    let modal = document.getElementById(mn);
+
+    if (typeof modal == 'undefined' || modal === null)
+        return;
+
+    modal.style.display = 'Block';
+    document.body.style.overflow = 'hidden';
+}
+
+function closeModal(mn) {
+    let modal = document.getElementById(mn);
+
+    if (typeof modal == 'undefined' || modal === null)
+        return;
+
+    modal.style.display = 'none';
+    document.body.style.overflow = 'auto';
+}
+/*Fim script Modal*/
